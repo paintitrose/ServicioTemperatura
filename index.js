71896573b4f205556379738412ba1a8f216f7717
paintitrose <cris.ohlin@gmail.com>
2024-06-08 18:12:49 -0500

    Tercer Commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = 8098;
 const server = createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Alumnos UPN: Dayner Osores, Diego del Piero Tinco, Axel Solis Delgado y Chris Vega');
+    res.end('Alumnos UPN:\n1. Dayner Osores\n2.Diego del Piero Tinco\n3.Axel Solis Delgado\n4.Chris Vega');
 });
 
 server.listen(port, hostname, () => {
